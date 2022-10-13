16582ad4683121d2b024786b4a3f07c032c665e8
Skyler Ferreira <sky_3489@icloud.com>
2022-10-13 15:32:52 -0700

    created stpeDef schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,38 @@
 const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+ type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    bookCount: String!
+    skills: [Book]
+  }
 
+  type Book {
+    authors: [User]
+    description: String!
+    bookId: ID!
+    image: String
+    link: String
+    title: String! 
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, bookId: ID!, image: String!, link: String!, title: String!): User
+    removeBook(bookId: ID!): User
+  }
+`
 module.exports = typeDefs;
